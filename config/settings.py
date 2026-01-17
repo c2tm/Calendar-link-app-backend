01eeccdb2d2d88b4d9dd4b91ab18468163a549ea
c2tm <cyork4839@gmail.com>
2026-01-17 17:20:43 -0500

    added railway to allowed hosts, created django secret key

--- config/settings.py
+++ config/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3&0h--si3@1vhj%0z-!b*!cu)67gsv+7@6rq8pgw+x$&1r5izn'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'shaftless-adriana-agatoid.ngrok-free.dev']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'calendar-link-app-backend-production.up.railway.app']
 
 # Application definition
 
